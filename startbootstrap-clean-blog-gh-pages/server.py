625a3826f4b437856af4de112a7e7ebe493f827a
Abhinav1303 <cbabhinav@gmail.com>
2024-05-16 11:31:30 +0800

    Removed a function

--- startbootstrap-clean-blog-gh-pages/server.py
+++ startbootstrap-clean-blog-gh-pages/server.py
@@ -36,12 +36,6 @@ def contact():
     return render_template("contact.html", msg_sent=False)
 
 
-def send_email(name, email, phone, message):
-    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
-    with smtplib.SMTP("smtp.gmail.com") as connection:
-        connection.starttls()
-        connection.login(OWN_EMAIL, OWN_PASSWORD)
-        connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message)
 
 
 if(__name__=="__main__"):
